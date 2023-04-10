b58dadb23b3849a821f8944b2a7355e026d225ca
Rayhan Mahmud <99743908+RayhanMahmud256@users.noreply.github.com>
2023-04-10 23:55:20 +0600

    Add files via upload

---
+++ donation.js
@@ -0,0 +1,23 @@
+function get1() {
+    document.getElementById("input").setAttribute('value', 500);
+  }
+
+  function get2() {
+    document.getElementById("input").setAttribute('value', 1000);
+  }
+
+  function get3() {
+    document.getElementById("input").setAttribute('value', 2000);
+  }
+
+  function get4() {
+    document.getElementById("input").setAttribute('value', 5000);
+  }
+
+  function get5() {
+    document.getElementById("input").setAttribute('value', 10000);
+  }
+
+  function get6() {
+    document.getElementById("input").setAttribute('value', 50000);
+  }
